27bf5baf018c1947e046f5509f84fd779b03871d
lanarokip <lankipkoecharo@gmail.com>
2021-07-23 00:26:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// this a jsquery code creating new clases thst aligns the text to the center
+$(document) .ready(function(){
+    $('div.jumbotro').addClass("text-center")
+    $('div.jumbotro').addClass("text-white")
+    $('div.about').addClass("text-center")
+    $('div.services').addClass("text-center text-white mt-5")
+});
